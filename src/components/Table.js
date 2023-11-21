636b29848f9398214f83eb70cb82318390495f89
Onejohi <tonnie254@live.com>
2023-11-21 15:07:09 +0300

    - create the stats component - create a re-usable table component -  highlight the current page that's active from the side nav

---
+++ src/components/Table.js
@@ -0,0 +1,41 @@
+export default function Table(props) {
+  return (
+    <div className="border-b border-gray-200 rounded-lg shadow bg-white px-4 py-5 sm:px-6">
+      <div className="sm:flex sm:items-center">
+        <h1 className="font-semibold text-1xl">
+          {props.tableTitle}
+        </h1>
+      </div>
+      <div className="mt-8 flow-root mb-10">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <div className="overflow-hidden ring-1 ring-black ring-opacity-5">
+              <table className="min-w-full divide-y divide-gray-300">
+                <thead className="bg-gray-100">
+                  <tr className="divide-x divide-gray-200">
+                    {props.theaders.map((header) => (
+                      <th scope="col" className={header.classes}>
+                        {header.title}
+                      </th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200 bg-white">
+                  {props.data.map((item, index) => (
+                    <tr className="divide-x divide-gray-200" key={index}>
+                      {Object.keys(item).map((itemkey) => (
+                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm text-gray-600 sm:pl-6">
+                          {item[itemkey]}
+                        </td>
+                      ))}
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/VaccinationSchedule.js
+++ src/pages/VaccinationSchedule.js
@@ -1,7 +1,24 @@
-function VaccinationSchedule() {
+import Table from '../components/Table';
+
+const schedules = [
+  { dose: '1', title: 'Oxford/AstraZeneca', schedule: 'Sat Jun 20 2023' },
+]
+
+const tableHeaders = [
+  { title: 'Dose', classes: 'py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6' },
+  { title: 'Vaccine', classes: 'px-3 py-3.5 text-left text-sm font-semibold text-gray-900' },
+  { title: 'Schedule', classes: 'px-3 py-3.5 text-left text-sm font-semibold text-gray-900' }
+]
+
+export default function VaccinationSchedule() {
   return (
-    <div>Welcome to Vaccination Schedule</div>
+    <>
+      <br />
+
+      <Table
+        tableTitle="Vaccination Schedule"
+        theaders={tableHeaders}
+        data={schedules} />
+    </>
   )
 }
-
-export default VaccinationSchedule;
