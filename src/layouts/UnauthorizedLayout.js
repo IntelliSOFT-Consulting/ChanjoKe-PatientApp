c250731635f4a313491651602c58718f1b16eff4
Onejohi <tonnie254@live.com>
2023-11-22 09:44:49 +0300

    - display chanjoke image when signing up

--- src/layouts/UnauthorizedLayout.js
+++ src/layouts/UnauthorizedLayout.js
@@ -1,14 +1,15 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import ChanjoKE from '../assets/chanjoke.png'
 
 const Layout = () => {
   return (
-    <>
+    <div className="grid grid-cols-2 h-full">
       <nav>
-        ChanjoKE
+        <img src={ChanjoKE} className="h-full" />
       </nav>
 
       <Outlet />
-    </>
+    </div>
   )
 };
 
