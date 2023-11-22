b32b749b26523dbac8d4ae1bd2be866e1a3face3
onejohi <tonnie254@live.com>
2023-11-22 17:43:23 +0300

    Initialize React App and dockerize the application (#1)
    * Initialize React application
    
    * - add a docker ignore and docker compose file
    - install Tailwind CSS to the project
    
    * - fix docker bugs caused by node version
    
    * - create environments for production and development (Docker)
    
    * fix docker configs
    
    ---------
    
    Co-authored-by: Patrick Nyatindo <nyatindopatrick@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Welcome to...
+        </p>
+        <a
+          className="text-3xl font-bold underline"
+          href="/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ChanjoKE
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
