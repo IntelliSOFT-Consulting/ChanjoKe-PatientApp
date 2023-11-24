c1b74b848dc53efe20e867268a72ab856cf1e62d
onejohi <tonnie254@live.com>
2023-11-24 12:31:07 +0300

    [chore] - Modularize table and stat component (#3)
    * - install React router
    - add basic routes for all pages
    - create an authorized, and unauthorized layout for the two app states
    - add basic components into their respective apps
    
    * - install hero icons and headless UI
    - import google material icons
    - create a navbar and reuse in the application
    - develop the side navigation and match it with the UI mockup
    
    * - create the stats component
    - create a re-usable table component
    -  highlight the current page that's active from the side nav
    
    * [config] - Add all basic routes, layout design and basic UI components for route navigation (#2)
    
    * - install React router
    - add basic routes for all pages
    - create an authorized, and unauthorized layout for the two app states
    - add basic components into their respective apps
    
    * - install hero icons and headless UI
    - import google material icons
    - create a navbar and reuse in the application
    - develop the side navigation and match it with the UI mockup
    
    * - use a file to store all routes
    - profile dropdown converted to it's own component
    - implement a root file for app
    - switch files to jsx that contain react code
    
    * - switch all href attributes under Link to "to=''"
    
    * - add chanjoKE logo to side navigation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,27 @@
 import { Link } from "react-router-dom";
+import Stats from '../components/Stats';
+import Table from '../components/Table';
+
+const appointments = [
+  { date: 'Tue, March 30, 2023', vaccine: 'Oxford/AstraZeneca' },
+]
+
+const tableHeaders = [
+  { title: 'Date', classes: 'py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6' },
+  { title: 'Vaccine', classes: 'px-3 py-3.5 text-left text-sm font-semibold text-gray-900' }
+]
 
 function Home() {
   return (
     <div>
-      <div>Welcome Home</div>
+      <Stats />
+
+      <br />
+
+      <Table
+        tableTitle="Upcoming appointments"
+        theaders={tableHeaders}
+        data={appointments} />
     </div>
   );
 }
