9517ca56513798f82d00ab8db1eed245a43477ae
Patrick Nyatindo <nyatindopatrick@gmail.com>
2024-09-30 16:27:07 +0300

    Develop (#53)
    * Updae build folder
    
    * Use node 20
    
    * Build image on prod
    
    * Refactor workflows
    
    * Fix matrix error
    
    * change fhir endpoint

--- src/hooks/useCertificates.js
+++ src/hooks/useCertificates.js
@@ -1,57 +1,55 @@
-import { useState } from "react"
-import { useApiRequest } from "../api/useApi"
-import moment from "moment"
+import { useState } from "react";
+import { useApiRequest } from "../api/useApi";
+import moment from "moment";
 
-const certificateEndpoint = '/hapi/fhir/DocumentReference'
+const certificateEndpoint = "/chanjo-hapi/fhir/DocumentReference";
 
 export default function useCertificate() {
+  const { get } = useApiRequest();
 
-  const { get } = useApiRequest()
-
-  const [certificates, setCertificates] = useState([])
-  const [certificateCount, setCertificateCount] = useState(0)
-  const [loader, setLoader] = useState(false)
+  const [certificates, setCertificates] = useState([]);
+  const [certificateCount, setCertificateCount] = useState(0);
+  const [loader, setLoader] = useState(false);
 
   function getDisplayForSystemContainingMOH(items) {
-    const itemWithMOHSystem = items?.find(item => item.system.includes('MOH'));
+    const itemWithMOHSystem = items?.find((item) => item.system.includes("MOH"));
     return itemWithMOHSystem ? itemWithMOHSystem.display : undefined;
-}
+  }
 
   const fetchUserCertificates = async (user) => {
-    setLoader(true)
-    const response = await get(`${certificateEndpoint}?subject=Patient/${user?.fhirPatientId}&type:code=82593-5`)
+    setLoader(true);
+    const response = await get(`${certificateEndpoint}?subject=Patient/${user?.fhirPatientId}&type:code=82593-5`);
 
     if (response && Array.isArray(response?.entry) && response?.entry.length > 0) {
       const uniqueVaccines = new Set();
       const certificateData = response?.entry.reduce((accumulator, item) => {
         const vaccineName = getDisplayForSystemContainingMOH(item?.resource?.type?.coding);
-        const formattedDate = moment(item?.resource?.date).format('DD-MM-YYYY');
+        const formattedDate = moment(item?.resource?.date).format("DD-MM-YYYY");
         const attachmentData = item?.resource?.content?.[0]?.attachment;
         if (vaccineName && !uniqueVaccines.has(vaccineName)) {
           uniqueVaccines.add(vaccineName);
-    
+
           accumulator.push({
             vaccine: vaccineName,
             date: formattedDate,
-            action: { text: 'Download', data: attachmentData },
+            action: { text: "Download", data: attachmentData },
           });
         }
-    
+
         return accumulator;
       }, []);
-      setCertificates(certificateData)
-      setCertificateCount(certificateData?.length)
-      setLoader(false)
+      setCertificates(certificateData);
+      setCertificateCount(certificateData?.length);
+      setLoader(false);
     } else {
-      setLoader(false)
+      setLoader(false);
     }
-
-  }
+  };
 
   return {
     loader,
     certificates,
     certificateCount,
     fetchUserCertificates,
-  }
+  };
 }
--- src/hooks/useImmunization.js
+++ src/hooks/useImmunization.js
@@ -1,28 +1,27 @@
-import { useState } from "react"
-import { useApiRequest } from "../api/useApi"
+import { useState } from "react";
+import { useApiRequest } from "../api/useApi";
 
-const immunizationEndpoint = '/hapi/fhir/Immunization'
+const immunizationEndpoint = "/chanjo-hapi/fhir/Immunization";
 
 export default function useImmunization() {
-  const { get } = useApiRequest()
+  const { get } = useApiRequest();
 
-  const [immunizationCount, setImmunizationCount] = useState(0)
-  const [immunizations, setImmunizations] = useState([])
+  const [immunizationCount, setImmunizationCount] = useState(0);
+  const [immunizations, setImmunizations] = useState([]);
 
   const fetchPatientImmunizations = async (user) => {
-    const response = await get(`${immunizationEndpoint}?patient=Patient/${user?.fhirPatientId}&_count=100`)
+    const response = await get(`${immunizationEndpoint}?patient=Patient/${user?.fhirPatientId}&_count=100`);
 
     if (response?.entry && Array.isArray(response?.entry) && response?.entry.length > 0) {
-      const completedVaccines = response?.entry?.filter((item) => item?.resource?.status === 'completed')
-      setImmunizationCount(completedVaccines.length)
-      setImmunizations(response?.entry)
+      const completedVaccines = response?.entry?.filter((item) => item?.resource?.status === "completed");
+      setImmunizationCount(completedVaccines.length);
+      setImmunizations(response?.entry);
     }
-  }
+  };
 
   return {
     immunizations,
     immunizationCount,
     fetchPatientImmunizations,
-  }
-
+  };
 }
