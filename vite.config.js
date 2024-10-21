43079aaae772e6c26bc401b60099d943ddfb5044
nyatindopatrick <nyatindopatrick@gmail.com>
2024-10-21 09:02:38 +0300

    Add client build

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
+export default defineConfig(({command, mode}) => {
+  const config = {
+    plugins: [react()],
+    define: {
     "process.env": process.env,
   },
   server: {
     port: 3000,
-  },
+    },
+  };
+
+  if (command === "build") {
+    config.base = "/client/";
+  }
+
+  return config;
 });
